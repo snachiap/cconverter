af711fb3191325328b0b2b6651eac003e92455af
Santhosh <santhoshmani30@gmail.com>
2020-02-12 19:34:20 +0200

    gitignore added

--- src/main/java/com/smn/cconverter/config/AppConfig.java
+++ src/main/java/com/smn/cconverter/config/AppConfig.java
@@ -1,5 +1,10 @@
 package com.smn.cconverter.config;
 
+import io.github.sercasti.tracing.config.TracingConfig;
+import io.github.sercasti.tracing.core.Tracing;
+import io.github.sercasti.tracing.filter.TracingFilter;
+import io.github.sercasti.tracing.interceptor.TracingInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -8,5 +13,20 @@ public class AppConfig {
     public final static String EXCHANGE_RATES_CACHE = "ratesCache";
     public final static String VALID_CURRENCIES_CACHE = "currencies";
     public final static String BASE_CURRENCY = "EUR";
+
+    @Bean
+    protected Tracing tracing() {
+        return TracingConfig.createTracing();
+    }
+
+    @Bean
+    protected TracingFilter tracingFilter() {
+        return new TracingFilter();
+    }
+
+    @Bean
+    protected TracingInterceptor tracingInterceptor() {
+        return new TracingInterceptor(tracing());
+    }
 
 }
